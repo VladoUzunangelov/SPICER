abeba1b7f7fb77059aaabc4c005f611c71dcd5be
Vlado Uzunangelov <uzunangelov@gmail.com>
2020-04-17 22:21:15 -0700

    changed the name of the R version of kernel_norm from kernel.norm to kernel_norm

--- R/Spicer-funcs.R
+++ R/Spicer-funcs.R
@@ -3,17 +3,17 @@
 ## Evaluates the kernel norm of a given vector u for each kernel Also returns dot of each kernel with vector u (cached because frequently used in other
 ## computations) K is an NxNxM array where N - no. samples, M- no.kernels u - NxM matrix of M Nx1 vectors, whose kernel norm is evaluated for the kernel
 ## of matching index activeset defaults to all kernel indices is actiset is a vector of length zero, returns results with one dimension being zero
-kernel.norm <- function(K, u, activeset = NULL) {
+kernel_norm <- function(K, u, activeset = NULL) {
     if (is.null(activeset)) {
         activeset <- 1:dim(u)[2]
     }
-    
-    
+
+
     N <- dim(u)[1]
     M <- length(activeset)
     wdot <- matrix(0, N, M)
     normj <- rep(0, M)
-    
+
     if (M > 0) {
         for (i in 1:length(activeset)) {
             ind <- activeset[i]
